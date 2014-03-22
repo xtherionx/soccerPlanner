ed761e273e64736bb270ff6479b8f77791ef15fa
xtherionx <x_therion_x1@yahoo.com.mx>
2014-03-22 16:28:49 -0500

    Initial Commit

---
+++ soccerPlanner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  soccerPlanner
+//
+//  Created by Sergio Quintero Gómez on 3/22/14.
+//  Copyright (c) 2014 xtherionx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "soccerPlannerAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([soccerPlannerAppDelegate class]));
+    }
+}
---
+++ soccerPlanner/soccerPlannerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  soccerPlannerAppDelegate.h
+//  soccerPlanner
+//
+//  Created by Sergio Quintero Gómez on 3/22/14.
+//  Copyright (c) 2014 xtherionx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface soccerPlannerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
